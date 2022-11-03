c8b8df68fd529b85fb99230bc0e7b62db8244c98
Christianvisl <113419649+Christianvisl@users.noreply.github.com>
2022-11-03 12:35:41 +0100

    Add files via upload

--- Uke44/js-practise.js
+++ Uke44/js-practise.js
@@ -1,5 +1,3 @@
-
-
 var mattestykket = String(prompt("Skriv mattestykket"));
 var resultat;
 var tall1;
@@ -9,43 +7,52 @@ var operator;
 if (mattestykket.indexOf("+") > 0) {
     operator = mattestykket.indexOf("+");
     tall1 = Number(mattestykket.substring(0, operator));
-    tall2 = Number(mattestykket.substring(operator + 1, 100));
+    tall2 = Number(mattestykket.substring(operator + 1));
     resultat = tall1 + tall2;
     tall1 = String(tall1);
     tall2 = String(tall2);
     operator = mattestykket[mattestykket.indexOf("+")];
-    document.write(tall1 + " " + operator + " " + tall2 + " = " + resultat)
+    document.write(`${tall1} ${operator} ${tall2} = ${resultat}`);
 }
-
 else if (mattestykket.indexOf("-") > 0) {
     operator = mattestykket.indexOf("-");
     tall1 = Number(mattestykket.substring(0, operator));
-    tall2 = Number(mattestykket.substring(operator + 1, 20));
+    tall2 = Number(mattestykket.substring(operator + 1));
     resultat = tall1 - tall2;
     tall1 = String(tall1);
     tall2 = String(tall2);
     operator = mattestykket[mattestykket.indexOf("-")];
-    document.write(tall1 + " " + operator + " " + tall2 + " = " + resultat)
+    document.write(`${tall1} ${operator} ${tall2} = ${resultat}`);
 }
 else if (mattestykket.indexOf("*") > 0) {
     operator = mattestykket.indexOf("*");
     tall1 = Number(mattestykket.substring(0, operator));
-    tall2 = Number(mattestykket.substring(operator + 1, 20));
+    tall2 = Number(mattestykket.substring(operator + 1));
     resultat = tall1 * tall2;
     tall1 = String(tall1);
     tall2 = String(tall2);
     operator = mattestykket[mattestykket.indexOf("*")];
-    document.write(tall1 + " " + operator + " " + tall2 + " = " + resultat)
+    document.write(`${tall1} ${operator} ${tall2} = ${resultat}`);
 }
 else if (mattestykket.indexOf("/") > 0) {
     operator = mattestykket.indexOf("/");
     tall1 = Number(mattestykket.substring(0, operator));
-    tall2 = Number(mattestykket.substring(operator + 1, 20));
+    tall2 = Number(mattestykket.substring(operator + 1));
     resultat = tall1 / tall2;
     tall1 = String(tall1);
     tall2 = String(tall2);
     operator = mattestykket[mattestykket.indexOf("/")];
-    document.write(tall1 + " " + operator + " " + tall2 + " = " + resultat)
+    document.write(`${tall1} ${operator} ${tall2} = ${resultat}`);
+}
+else if (mattestykket.indexOf("^") > 0) {
+    operator = mattestykket.indexOf("^");
+    tall1 = Number(mattestykket.substring(0, operator));
+    tall2 = Number(mattestykket.substring(operator + 1));
+    resultat = tall1 ** tall2;
+    tall1 = String(tall1);
+    tall2 = String(tall2); 
+    operator = mattestykket[mattestykket.indexOf("^")];
+    document.write(`${tall1} ${operator} ${tall2} = ${resultat}`);
 }
 else {
     document.write("ERROR")
