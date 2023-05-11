883f05b91b7107e4dc5336fda9c660c110806dde
Christianvisl <113419649+Christianvisl@users.noreply.github.com>
2023-05-11 14:33:12 +0200

    Add files via upload

---
+++ OppgaverTimene/uke19-Russekort/database.js
@@ -0,0 +1,9 @@
+let data = {
+    kortArray: [
+        { navn: "Harald Rex V", adresse: "Slottet", tlf: "123 45 678", email: "majestet.kong.harald@kongehuset.no" },
+        { navn: "Petter Northug", adresse: "Trondhjem", tlf: "420 69 420", email: "petter.norTHUG@life.com" },
+        { navn: "Erna Solberg", adresse: "Høyregate 12a", tlf: "23 34 45 56", email: "ernablaabaer@stortinget.no" },
+        { navn: "Greta Thunberg", adresse: "Sverige", tlf: "Telefoner är klimafiendtliga", email: "skolstrejkFor@klimaet.se" },
+        { navn: "Leonardo daVinci", adresse: " Chapel of Saint-Hubert, Amboise, Frankrike", tlf: "Duuh?", email: "Leonardo.200IQ@science.it" }
+    ]
+}
