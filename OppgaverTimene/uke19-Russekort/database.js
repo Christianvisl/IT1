a38e2523a93c44daeb2c9786e6f4341b708f1eb6
Christianvisl <113419649+Christianvisl@users.noreply.github.com>
2023-05-25 14:56:03 +0200

    Add files via upload

--- OppgaverTimene/uke19-Russekort/database.js
+++ OppgaverTimene/uke19-Russekort/database.js
@@ -1,9 +1,9 @@
 let data = {
     kortArray: [
-        { navn: "Harald Rex V", adresse: "Slottet", tlf: "123 45 678", email: "majestet.kong.harald@kongehuset.no", bilde: "kongHaraldV.png" },
-        { navn: "Petter Northug", adresse: "Trondhjem", tlf: "420 69 420", email: "petter.norTHUG@life.com", bilde: "petterNorthug.png" },
-        { navn: "Erna Solberg", adresse: "Høyregate 12a", tlf: "23 34 45 56", email: "ernablaabaer@stortinget.no", bilde: "ernaSolberg.png" },
-        { navn: "Greta Thunberg", adresse: "Sverige", tlf: "Telefoner är klimafiendtliga", email: "skolstrejkFor@klimaet.se", bilde: "gretaThunberg.png" },
-        { navn: "Leonardo daVinci", adresse: " Chapel of Saint-Hubert, Amboise, Frankrike", tlf: "Duuh?", email: "Leonardo.200IQ@science.it", bilde: "leonardoDaVinci.png" }
+        { navn: "Harald Rex V", adresse: "Slottet", tlf: "tlf: 123 45 678", email: "mail: majestet.kong.harald@kongehuset.no", bilde: "kongHaraldV.png" },
+        { navn: "Petter Northug", adresse: "Trondhjem", tlf: "tlf: 420 69 420", email: "mail: petter.norTHUG@life.com", bilde: "petterNorthug.png" },
+        { navn: "Erna Solberg", adresse: "Høyregate 12a", tlf: "tlf: 23 34 45 56", email: "mail: ernablaabaer@stortinget.no", bilde: "ernaSolberg.png" },
+        { navn: "Greta Thunberg", adresse: "Sverige", tlf: "tlf: Telefoner är klimafiendtliga", email: "mail: greta@klima.se", bilde: "gretaThunberg.png" },
+        { navn: "Leonardo daVinci", adresse: "Chapel of Saint-Hubert, Amboise, Frankrike", tlf: "tlf: Duuh?", email: "mail: fantes ikke", bilde: "leonardoDaVinci.png" }
     ]
 }
